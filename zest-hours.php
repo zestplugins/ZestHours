bbf2b9fcc548547728aadf853df81acf0f1659d7
lewis ushindi <frenziecodes@gmail.com>
2023-10-06 08:59:07 +0300

    initial commit

---
+++ zest-hours.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @link              https://github.com/zestplugins/ZestHours
+ * @since             1.0.0
+ * @package           Business hours management
+ *
+ * @wordpress-plugin
+ * Plugin Name:       ZestHours - Business hours management
+ * Plugin URI:        https://github.com/zestplugins/ZestHours
+ * Description:       Effortlessly display and manage business hours on your website.
+ * Version:           0.1.0
+ * Author:            zestplugins
+ * Author URI:        https://github.com/zestplugins/ZestHours
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       zest-hours
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+    die;
+}
+
+/**
+ * Current plugin version.
+ */
+define( 'Zest_HOURS_VERSION', '0.1.0' );
