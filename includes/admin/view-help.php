01dfbf8c656363f85aa3b3bb64c2e74ede508bb2
lewis ushindi <frenziecodes@gmail.com>
2023-10-09 12:27:37 +0300

    creating help page

--- includes/admin/view-help.php
+++ includes/admin/view-help.php
@@ -41,9 +41,111 @@ if ( ! class_exists( 'ZestHoursMenuHelpPage' ) ) {
 		 */
 		public function menu_help_page() {
 			?>
-			<h2><?php esc_html_e( 'Help Page', 'zesthours' ); ?></h2>
+			<section style="background-color: #fdfdfd; margin-right: 15px; margin-top: 15px; min-height: 100svh;">
+				<div class="zesthours-help-main">
+					<h1 style="color: whitesmoke;"><?php esc_html_e( 'ZestHours', 'zesthours' ); ?></h1>
+				</div>            
+					
+				<div class="zesthours-help-tabs">
+					<ul class="zesthours-help-tab-links">
+						<li class="zesthours-help-tab-active"><a href="#zesthours-welcome-tab"><?php esc_html_e( 'Welcome', 'zesthours' ); ?></a></li>
+						<li><a href="#zesthours-support-tab"><?php esc_html_e( 'Support', 'zesthours' ); ?></a></li>
+					</ul>
+			
+					<div class="zesthours-help-tab-content">
+						<div id="zesthours-welcome-tab" class="zesthours-help-tab zesthours-help-tab-active">
+							<div>
+								<h3><?php esc_html_e( 'Thank You For choosing ZestHours.', 'zesthours' ); ?></h3>
+								<p><?php esc_html_e( 'Better, beautiful and flexible business hours management and display.', 'zesthours' ); ?></p>
+							</div>
+						</div>
+			
+						<div id="zesthours-support-tab" class="zesthours-help-tab">
+							<h2><?php esc_html_e( 'Support', 'zesthours' ); ?></h2>
+							<!-- Add support content here -->
+						</div>
+					</div>
+				</div>
+			</section>
+			
+			<style>
+				/* Style for tabs */
+				.zesthours-help-tabs {
+					font-family: Arial, sans-serif;
+					padding-left: 10px;
+				}
+		
+				.zesthours-help-main {
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+					align-items: center;
+					text-align: center;
+					background-color: black;
+					min-height: 100px;
+				}
+		
+				.zesthours-help-tab-links {
+					display: flex;
+					list-style: none;
+					padding: 0;
+					margin: 0;
+				}
+		
+				.zesthours-help-tab-links li {
+					margin-right: 10px;
+				}
+		
+				.zesthours-help-tab-links a {
+					text-decoration: none;
+					background-color: #f2f2f2;
+					padding: 10px 20px;
+					border: 1px solid #ccc;
+					border-radius: 5px;
+				}
+		
+				.zesthours-help-tab-links a:hover {
+					background-color: #ddd;
+				}
+		
+				.zesthours-help-tab-links .zesthours-help-tab-active a {
+					background-color: #fff;
+					border: 1px solid #ddd;
+				}
+		
+				/* Style for tab content */
+				.zesthours-help-tab {
+					display: none;
+				}
+		
+				.zesthours-help-tab-active {
+					display: block;
+				}
+			</style>
+		
+			<script>
+				document.addEventListener("DOMContentLoaded", function() {
+					const tabLinks = document.querySelectorAll(".zesthours-help-tab-links a");
+					const tabContents = document.querySelectorAll(".zesthours-help-tab-content .zesthours-help-tab");
+		
+					tabLinks.forEach((link) => {
+						link.addEventListener("click", function (e) {
+							e.preventDefault();
+							tabLinks.forEach((l) => l.parentElement.classList.remove("zesthours-help-tab-active"));
+							this.parentElement.classList.add("zesthours-help-tab-active");
+		
+							const targetTab = document.querySelector(this.getAttribute("href"));
+							tabContents.forEach((tab) => tab.classList.remove("zesthours-help-tab-active"));
+							targetTab.classList.add("zesthours-help-tab-active");
+						});
+					});
+				});
+			</script>
+		
 			<?php
 		}
+		
+		
 	}
 }
 
