7a2a5eb46202fc12d6085815ba551419838dab42
lewis ushindi <frenziecodes@gmail.com>
2023-10-09 10:15:09 +0300

    add custom action links

--- zest-hours.php
+++ zest-hours.php
@@ -29,3 +29,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'ZESTHOURS_VERSION', '0.1.0' );
 
 require plugin_dir_path( __FILE__ ) . 'includes/includes.php';
+
+// Add custom action link to the plugin's action links
+function zesthours_management_add_actions_link( $links ) {
+    $settings_link = '<a href="' . admin_url( 'admin.php?page=zesthours_menu_settings' ) . '">' . esc_html__( 'Settings', 'zesthours' ) . '</a>';
+    $help_link = '<a href="' . admin_url( 'admin.php?page=zesthours_menu_help' ) . '">' . esc_html__( 'Help', 'zesthours' ) . '</a>';
+    array_push( $links, $settings_link, $help_link );
+    return $links;
+}
+add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'zesthours_management_add_actions_link' );
